e76cd83ba240666d81dcfafee01effc9833bcb43
Camilo Ernesto Quea Montesinos <camiloquea98@gmail.com>
2022-07-04 03:10:58 -0500

    Verificacion de inicializacion implementada

--- src/services/db.ts
+++ src/services/db.ts
@@ -11,45 +11,57 @@ import { UserExample } from '../components/Users/UserExample';
 import { UserModel } from '../components/Users/UserSchema';
 import { faker } from '@faker-js/faker'
 
+const createData = async () => {
+    // mongoose.connection.db.dropDatabase();
+
+    //VERIFICAR SI HAY DATOS
+    const verify = await MedicalHistorytModel.find();
+
+    //SI HAY DATOS CANCELAR INCIALIZACION
+    if (verify.length !== 0) return;
+
+
+    //AGREGANDO DEPARTAMENTOS
+
+    const departmentList = await DepartmentModel.insertMany(DepartmentExample);
+
+    //AGREGANDO USUARIOS
+
+    //---AGREGANDO LOS DEPARTAMENTOS GENERADOS
+    const UserModified = UserExample.map((user) => {
+
+        const departmentSelected = departmentList[Math.floor(Math.random() * departmentList.length)];
+
+        user.department = {
+            _id: departmentSelected._id,
+            name: departmentSelected.name
+        }
+
+        return { ...user, _id: undefined };
+
+    })
+
+
+    //INSERTANDO USUARIOS
+    const userList = await UserModel.insertMany(UserModified)
+
+    //INGRESANDO PACIENTES
+    const patientsList = await PatientModel.insertMany(PatientExample)
+
+
+    //CREACION DE HISTORIAS
+    const historiesList = listHistories(departmentList, patientsList, userList, 1000)
+
+
+    //INSERTANDO LAS HISTORIAS
+    await MedicalHistorytModel.insertMany(historiesList)
+}
+
 export const initDB = async () => {
 
     mongoose.connect('mongodb://localhost:27017/NEWtest', async () => {
-        // mongoose.connection.db.dropDatabase();
 
-        //AGREGANDO DEPARTAMENTOS
-
-        const departmentList = await DepartmentModel.insertMany(DepartmentExample);
-
-        //AGREGANDO USUARIOS
-
-        //---AGREGANDO LOS DEPARTAMENTOS GENERADOS
-        const UserModified = UserExample.map((user) => {
-
-            const departmentSelected = departmentList[Math.floor(Math.random() * departmentList.length)];
-
-            user.department = {
-                _id: departmentSelected._id,
-                name: departmentSelected.name
-            }
-
-            return { ...user, _id: undefined };
-
-        })
-
-
-        //INSERTANDO USUARIOS
-        const userList = await UserModel.insertMany(UserModified)
-
-        //INGRESANDO PACIENTES
-        const patientsList = await PatientModel.insertMany(PatientExample)
-
-
-        //CREACION DE HISTORIAS
-        const historiesList = listHistories(departmentList, patientsList, userList, 1000)
-
-
-        //INSERTANDO LAS HISTORIAS
-        await MedicalHistorytModel.insertMany(historiesList)
+        await createData();
 
     });
 
